2171dac708a1bb21a7537fda2d2ef0f957631a54
Dylan Osolian <dylan.osolian@gmail.com>
2020-09-07 10:35:52 +0200

    initial

---
+++ src/main/java/ProjectTemplate.java
@@ -0,0 +1,24 @@
+import controller.ProjectController;
+import model.Project;
+import view.ProjectView;
+import javax.swing.SwingUtilities;
+
+public final class ProjectTemplate {
+	private ProjectTemplate() {
+		/* No instances allowed! */
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				final Project project = new Project();
+				final ProjectView projectView = new ProjectView(project);
+
+				ProjectController.create(project, projectView);
+				projectView.setVisible(true);
+			}
+		});
+	}
+}
