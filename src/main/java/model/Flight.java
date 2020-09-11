5c4879b93a5b9f48db4b12229c6a3c7e1be4d014
Dylan Osolian <dylan.osolian@gmail.com>
2020-09-11 11:43:16 +0200

    started on flight class

--- src/main/java/model/Flight.java
+++ src/main/java/model/Flight.java
@@ -1,4 +1,14 @@
 package model;
 
+import java.util.Date;
+
 public class Flight {
+    private Date date;
+    private int nHours;
+    private int nMinutes;
+    private int nStarts;
+    private String place;
+    private String destination;
+    private String comment;
+
 }
