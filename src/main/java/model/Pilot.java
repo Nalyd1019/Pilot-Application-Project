dde101e3d35f9188e5b57d1d5811bb4ae443697b
Lisa <lisasamuelssons@gmail.com>
2020-09-14 19:57:26 +0200

    Lade till några instansvariabler i Pilot-klassen

--- src/main/java/model/Pilot.java
+++ src/main/java/model/Pilot.java
@@ -1,8 +1,16 @@
 package model;
 
+import sun.rmi.runtime.Log;
+
 import java.util.List;
 
 public class Pilot {
     private Logbook logbook;
     private List<License> licenses;
+    private String username;
+    private String name;
+    private String email;
+
+
 }
+
