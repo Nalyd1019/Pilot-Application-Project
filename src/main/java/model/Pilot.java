0ecd722c89edad4f662073e34d293d303aa1b9f3
Alburrito18 <albertlund18@gmail.com>
2020-09-22 10:42:25 +0200

    la till passwordauthentication
    al, uppdaterade även pilot klassen med den nya funktionaliteten samt gjorde tester så att logga in fortfarande funkar

--- src/main/java/model/Pilot.java
+++ src/main/java/model/Pilot.java
@@ -1,5 +1,7 @@
 package model;
 
+import data.PasswordAuthentication;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,21 +11,32 @@ public class Pilot {
     private String password;
     private String name;
     private String email;
+    private PasswordAuthentication passwordAuthentication = new PasswordAuthentication();
 
     public Pilot(String password1, String password2, String name, String email) {
         if (password1.equals(password2)) {               //så att användaren skriver in rätt lösen båda gångerna
-            password = password1;
+            password = passwordAuthentication.hash(password1.toCharArray());
             this.name = name;
             this.email = email;
         }
     }
+
+    /**
+     * Tests if the instance variable "name" is set
+     * @return true if it is set, else return false
+     */
     protected boolean nameSet(){
         return name!=null;
     }
 
+    /**
+     *  Method that checks if a given combination of email and password matches the data in the instance
+     * @param email the email the user tries to login with
+     * @param password the password the user tries to login with
+     * @return true if the given combination matches the data, else returns false
+     */
     public boolean validateLogin(String email, String password){
-        return this.email.equals(email) && this.password.equals(password);
+        return this.email.equals(email) && passwordAuthentication.authenticate(password.toCharArray(), this.password);
     }
-
 }
 
