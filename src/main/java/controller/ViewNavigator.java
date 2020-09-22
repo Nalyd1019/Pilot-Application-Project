5c21fbf66388d04f9f28815ad7f6207feacec79d
Samdahl <samdahl@protonmail.com>
2020-09-22 10:56:32 +0200

    viewnavigator update

---
+++ src/main/java/controller/ViewNavigator.java
@@ -0,0 +1,34 @@
+package controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import javax.swing.text.View;
+import java.io.IOException;
+import java.net.URL;
+
+public class ViewNavigator {
+    public static final String LOGIN = "loginPage.fxml";
+    public static final String START = "startPage.fxml";
+
+    private static Stage mainStage;
+
+
+    public static void setMainStage(Stage mainStage){
+        ViewNavigator.mainStage = mainStage;
+    }
+
+    public static void LoadView(String fxml){
+        try {
+            URL location = ViewNavigator.class.getResource(fxml);
+            Parent root = FXMLLoader.load(ViewNavigator.class.getClassLoader().getResource(fxml));
+            System.out.println(root);
+            mainStage.getScene().setRoot(root);
+        } catch (IOException e) {
+            System.out.println("Could not open fxmlPath: " + fxml);
+            e.printStackTrace();
+        }
+    }
+}
+
