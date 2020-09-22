0dd18b7626de0e70fd59d0076a70775a0c299bc8
Alburrito18 <albertlund18@gmail.com>
2020-09-22 11:24:07 +0200

    Update Pilot.java
    snabb kommentar fix

--- src/main/java/model/Pilot.java
+++ src/main/java/model/Pilot.java
@@ -15,7 +15,8 @@ public class Pilot {
 
     public Pilot(String password1, String password2, String name, String email) {
         if (password1.equals(password2)) {               //så att användaren skriver in rätt lösen båda gångerna
-            password = passwordAuthentication.hash(password1.toCharArray());
+            password = password1;
+       //     password = passwordAuthentication.hash(password1.toCharArray());
             this.name = name;
             this.email = email;
         }
@@ -36,7 +37,8 @@ public class Pilot {
      * @return true if the given combination matches the data, else returns false
      */
     public boolean validateLogin(String email, String password){
-        return this.email.equals(email) && passwordAuthentication.authenticate(password.toCharArray(), this.password);
+        return this.email.equals(email) && this.password.equals(password);
+  //      return this.email.equals(email) && passwordAuthentication.authenticate(password.toCharArray(), this.password);
     }
 }
 
