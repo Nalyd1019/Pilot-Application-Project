2037001349982d26b05875c951faa11e205174cc
Alburrito18 <albertlund18@gmail.com>
2020-10-01 09:48:02 +0200

    funkar nästan nu
    al jöööööö

--- src/main/java/controller/AccountController.java
+++ src/main/java/controller/AccountController.java
@@ -2,9 +2,14 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Control;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import model.FlightBuddy;
 import model.License;
+import model.Pilot;
 
 import java.net.URL;
 import java.util.Objects;
@@ -17,6 +22,11 @@ public class AccountController implements Initializable {
     @FXML private TextField emailTextField;
     @FXML private TextField medicalCertTextField;
     @FXML private TextField flightCertTextField;
+    @FXML private Label nameErrorLabel;
+    @FXML private Label passwordErrorLabel;
+    @FXML private Label emailErrorLabel;
+    @FXML private Label medicalCertErrorLabel;
+    @FXML private Label flightCertErrorLabel;
 
     private FlightBuddy flightBuddy = FlightBuddy.getInstance();
 
@@ -38,10 +48,48 @@ public class AccountController implements Initializable {
         return null;
     }
 
-    @FXML public void updateUserInfo(){
-        if (!flightBuddy.userExists(emailTextField.getText())
-                ||flightBuddy.getCurrentUser().getEmail().equals(emailTextField.getText())){
-        //TODO: Fixa så ny info kan sparas till användaren. Räcker det att skriva till currentuser?
+    @FXML public void updateUserInfo() {
+        Pilot pilot = flightBuddy.getCurrentUser();
+        if ((!flightBuddy.userExists(emailTextField.getText()) || pilot.getEmail().equals(emailTextField.getText()))&&!emptyTextField(emailTextField)) {
+            confirmedControlColorChange(emailTextField);
+            boolean newName = emptyTextField(nameTextField);
+            boolean newPassword = emptyTextField(passwordTextField);
+            boolean newMedical = emptyTextField(medicalCertTextField);
+            boolean newFlight = emptyTextField(flightCertTextField);
+            if (!newName&&!newPassword&&!newMedical&&!newFlight){
+                pilot.setName(nameTextField.getText());
+                pilot.setPassword(passwordTextField.getText());
+                pilot.setEmail(emailTextField.getText());
+                Objects.requireNonNull(getWantedLicense(License.MEDICAL)).setExpirationDate(medicalCertTextField.getText());
+                Objects.requireNonNull(getWantedLicense(License.FLIGHT)).setExpirationDate(flightCertTextField.getText());
+            }
         }
+        else {
+            errorControlColorChange(emailTextField);
+            emailErrorLabel.setText("Email redan registrerad");
+            errorLabelColorChange(emailErrorLabel);
+        }
+    }
+    //Dessa metoder till en abstrakt superklass?
+    private void errorControlColorChange(Control control){
+        controlColorChange(control, Color.RED);
+    }
+    private void confirmedControlColorChange(Control control){
+        controlColorChange(control,Color.GREEN);
+    }
+    private void controlColorChange(Control control, Color color){
+        control.setBorder(new Border(new BorderStroke(color, BorderStrokeStyle.SOLID,
+                CornerRadii.EMPTY,new BorderWidths(1))));
+    }
+    private void errorLabelColorChange(Label label){
+        label.setTextFill(Color.RED);
+    }
+    private boolean emptyTextField(TextField textField){
+        if (textField.getText().isEmpty()){
+            errorControlColorChange(textField);
+            return true;
+        }
+        confirmedControlColorChange(textField);
+        return false;
     }
 }
--- src/main/java/model/License.java
+++ src/main/java/model/License.java
@@ -56,6 +56,10 @@ public class License {
     public String getExpirationDate() {
         return expirationDate;
     }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }
 
 
