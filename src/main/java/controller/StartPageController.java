3ea03746b8e30015e67ccf42bf46379441314104
Dylan Osolian <dylan.osolian@gmail.com>
2020-10-02 13:48:06 +0200

    Connected button on startpage

--- src/main/java/controller/StartPageController.java
+++ src/main/java/controller/StartPageController.java
@@ -1,7 +1,10 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import model.FlightBuddy;
 
@@ -13,11 +16,19 @@ public class StartPageController implements Initializable {
     private FlightBuddy flightBuddy = FlightBuddy.getInstance();
 
     @FXML private Label welcomeLabel;
+    @FXML private Button bookButton;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         welcomeLabel.setText("Välkommen " + flightBuddy.getCurrentUser().getName());
+
+        bookButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ViewNavigator.LoadView(ViewNavigator.BOOKING);
+            }
+        });
 
     }
 }
