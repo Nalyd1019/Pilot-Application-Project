c8b05c0a87c1ea97c2bed7296f8e91db554dd8f8
Dylan Osolian <dylan.osolian@gmail.com>
2020-10-02 13:40:02 +0200

    Added functionality for welcome msg

--- src/main/java/controller/StartPageController.java
+++ src/main/java/controller/StartPageController.java
@@ -1,14 +1,23 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import model.FlightBuddy;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StartPageController implements Initializable {
 
+    private FlightBuddy flightBuddy = FlightBuddy.getInstance();
+
+    @FXML private Label welcomeLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        welcomeLabel.setText("Välkommen " + flightBuddy.getCurrentUser().getName());
 
     }
 }
