be18501ad10ad9f803dc02c6b9e511e4eab603a1
malinros <malinros@gmail.com>
2020-10-02 10:37:55 +0200

    Jag har gjort klart det grundläggande i FXML-dokumenten för bookingPage samt bookingItem. Jag har också lagt till en ny klass som är en bookingItemController.

--- src/main/java/controller/BookingController.java
+++ src/main/java/controller/BookingController.java
@@ -1,11 +1,21 @@
 package controller;
 
-public class BookingController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BookingController implements Initializable {
+
+    @FXML private Label flightHoursLabel;
+    @FXML private ScrollPane bookingScrollPane;
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
-
-
-
-
+    }
 }
