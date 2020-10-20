9f50702abeb71b98c820994a6ffcf8c2cb3c1301
Dylan Osolian <dylan.osolian@gmail.com>
2020-10-20 14:53:25 +0200

    Intergrated JDepend and made one generic interface adapter

--- src/main/java/dataService/IdataService.java
+++ src/main/java/dataService/IdataService.java
@@ -6,12 +6,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * @Author Dylan Osolian
+ * Interface that represents a data service which can both load and save data.
+ */
+
 public interface IdataService {
 
-    /**
-     * @Author Dylan Osolian
-     * Interface that represents a data service which can both load and save data.
-     */
     List<FlyingClub> load() throws FileNotFoundException;
     void save(List<FlyingClub> flyingClubs) throws IOException;
 
